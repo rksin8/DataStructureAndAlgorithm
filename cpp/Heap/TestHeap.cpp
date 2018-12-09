8df5e843443db611e87871816c373bf9c3aa2bf3
rksin8 <ranjeetbhu88@gmail.com>
2018-12-10 00:51:35 +1100

    heap done!

--- cpp/Heap/TestHeap.cpp
+++ cpp/Heap/TestHeap.cpp
@@ -16,10 +16,33 @@ int main(int argc, char *argv[])
 
     h.print();
 
+    h.deleteMin();
+
+    h.print();
+
+    Heap<int> h2;
+    h2.insert(6);
+    h2.insert(9);
+    h2.insert(5);
+    h2.insert(1);
+    h2.insert(4);
+    h2.insert(2);
+
+    h2.print();
+    h2.deleteMin();
+    h2.print();
+
+
+
+    /*
     std::vector<int> v{1,2,5,3,6,4};
     Heap<int> h2{v};
 
     h2.print();
 
+    h2.deleteMin();
+
+    h2.print();
+*/
     return 0;
 }
--- cpp/Heap/heap.h
+++ cpp/Heap/heap.h
@@ -7,11 +7,6 @@
 template <typename Comparable>
 class Heap
 {
-private:
-    int currentSize;
-    std::vector<Comparable> array;
-    void buildHeap();
-    void percolateDown(int hole);
 
 public:
     explicit Heap(int capacity = 100);
@@ -36,13 +31,44 @@ public:
         return index/2;
     }
     void print(){
-        for (auto& i : array) {
-           std::cout << i << " "; 
+        for (int i = 1; i < currentSize+1; ++i) {
+            
+           std::cout <<array[i] << " "; 
         }
         std::cout << std::endl;
     }
 
     virtual ~Heap(){}
+private:
+    int currentSize;
+    std::vector<Comparable> array;
+    void buildHeap(){
+        for (int i = currentSize/2; i > 0; --i) {
+            percolateDown(i);
+        }
+    }
+    void percolateDown(int hole){
+    int minIdx;
+    Comparable tmp = std::move(array[hole]);
+
+    for(; hole*2 <= currentSize; hole = minIdx){
+    
+       auto leftChildIdx = hole*2;  //left child index
+       auto rightChildIdx = leftChildIdx+1; // right child index
+       minIdx = leftChildIdx;
+        // Check minimum at leftIndex or RightIndex
+        if (leftChildIdx != currentSize && array[rightChildIdx] < array[leftChildIdx])
+            minIdx= rightChildIdx; //++ child; // move to right child
+
+        if (array[minIdx] < tmp)
+            array[hole] = std::move(array[minIdx]);
+        else
+            break;
+    }
+
+    array[hole] = std::move(tmp);
+
+    }
 };
 
 #endif /* HEAP_H */
@@ -50,14 +76,14 @@ public:
 
 template <typename Comparable>
 Heap<Comparable>::Heap(int capacity)
-    :array(capacity),currentSize{0}
+    :array(capacity+1),currentSize{0}
 {
 }
 
 
 template <typename Comparable>
 Heap<Comparable>::Heap(const std::vector<Comparable>& items)
-    :array(items.size()+10),currentSize(items.size()){
+    :array(items.size()+10), currentSize{items.size()}{
     for (int i = 0; i < items.size(); ++i) {
         array[i+1] = items[i];
     }
@@ -65,36 +91,12 @@ Heap<Comparable>::Heap(const std::vector<Comparable>& items)
 }
 
 template <typename Comparable>
-void Heap<Comparable>::buildHeap()
+bool Heap<Comparable>::isEmpty() const
 {
-    for (int i = currentSize/2; i > 0; --i) {
-        percolateDown(i);
-    }
+    return currentSize==0;
+
 }
 
-template <typename Comparable>
-void Heap<Comparable>::percolateDown(int hole)
-{
-    int child;
-    Comparable tmp = std::move(array[hole]);
-
-    for(; hole*2 <= currentSize; hole = child){
-    
-        child = hole*2;  //left child index
-
-        if (child != currentSize && array[child+1] < array[child]){
-            ++ child;
-        }
-
-        if (array[child] < tmp)
-            array[hole] = std::move(array[child]);
-        else
-            break;
-    }
-
-    array[hole] = std::move(tmp);
-
-}
 
 template <typename Comparable>
 void Heap<Comparable>::insert(const Comparable& x)
@@ -126,44 +128,50 @@ void Heap<Comparable>::insert(Comparable&& x)
     // Percolate up
     
     int hole = ++currentSize;
-
-    Comparable copy = x;
-
-    array[0] = std::move(copy);
-
-    for (; x < array[parentIndex(hole)]; hole/=2) {
+    for (; hole > 1 &&  x < array[parentIndex(hole)]; hole/=2) {
         array[hole] = std::move(array[parentIndex(hole)]);  // Swap 
     }
 
-    array[hole] = std::move(array[0]);
-}
-
-template <typename Comparable>
-bool Heap<Comparable>::isEmpty() const
-{
-    return currentSize;
-
+    array[hole] = std::move(x);
 }
 
 template <typename Comparable>
 const Comparable& Heap<Comparable>::findMin() const
 {
-    Comparable x = 5;
-    return x;
+
+    if(isEmpty()){
+        std::cout << "Empty Heap" << std::endl;
+    }else{
+        return array[1];
+    }
 }
 template <typename Comparable>
 void Heap<Comparable>::deleteMin()
 {
+    if(isEmpty()){
+        std::cout << "Empty heap...!" << std::endl;
+    }else{
+    array[1] = std::move(array[currentSize--]);
+    percolateDown(1);
+    }
 
 }
 template <typename Comparable>
 void Heap<Comparable>::deleteMin(Comparable& minItem){
+    if(isEmpty()){
+        std::cout << "Empty heap...!" << std::endl;
+    }else{
+    minItem = std::move(array[currentSize--]);
+    array[1] = std::move(array[currentSize--]);
+    percolateDown(1);
+    }
 
 }
+
 template <typename Comparable>
 void Heap<Comparable>::makeEmpty()
 {
-
+     currentSize =0;
 
 }
 
